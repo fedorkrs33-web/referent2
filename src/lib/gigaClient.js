cfb57c3883bca27b010ac6c8146636fd7f8fd7b1
fedorkrs33-web <fedorkrs.33@gmail.com>
2026-01-04 15:36:28 +0700

    gigaclient del

--- src/lib/gigaClient.js
+++
@@ -1,8 +0,0 @@
-// src/lib/gigaClient.js
-export async function getModels() {
-  const token = await getGigaChatToken();
-  const response = await axios.get('https://gigachat.devices.sberbank.ru/api/v1/models', {
-    headers: { 'Authorization': `Bearer ${token}` }
-  });
-  return response.data;
-}
